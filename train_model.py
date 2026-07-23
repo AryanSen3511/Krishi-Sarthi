b12b45640770d2b6778d0962636710cfb786e97c
Aryan Sen <senaryan973@gmail.com>
2026-07-23 17:23:44 +0530

    changes in ml file

--- train_model.py
+++ train_model.py
@@ -18,41 +18,42 @@ df = pd.read_csv("Krishi_ai_dataset/cleaned_crop_dataset.csv")
 # ================= FEATURES & TARGET =================
 
 X = df.drop("label", axis=1)
-
 y = df["label"]
 
 # ================= LABEL ENCODING =================
 
 le = LabelEncoder()
-
 y = le.fit_transform(y)
+
+# ================= TRAIN TEST SPLIT =================
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
 
 # ================= FEATURE SCALING =================
 
 scaler = StandardScaler()
 
-X_scaled = scaler.fit_transform(X)
-
-# ================= TRAIN TEST SPLIT =================
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled,
-    y,
-    test_size=0.2,
-    random_state=42
-)
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # ================= MODELS =================
 
 models = {
 
-    "Random Forest": RandomForestClassifier(),
+    "Random Forest": RandomForestClassifier(random_state=42),
 
     "SVM": SVC(probability=True),
 
     "KNN": KNeighborsClassifier(),
 
     "Naive Bayes": GaussianNB()
+
 }
 
 # ================= TRAINING & ACCURACY =================
@@ -89,9 +90,7 @@ print("====================================")
 # ================= SAVE FILES =================
 
 joblib.dump(best_model, "crop_model.pkl")
-
 joblib.dump(scaler, "scaler.pkl")
-
 joblib.dump(le, "label_encoder.pkl")
 
-print("\nModel, scaler and encoder saved successfully!")
+print("\nModel, Scaler and Label Encoder saved successfully!")
